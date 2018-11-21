7e47ac76c05cdc97beeb18401b01f38d500752be
dziubutkus <dziubutkus@gmail.com>
2018-11-21 13:08:22 -0700

    Revert "Merge branch 'Resettable-lottery-smart-contract' of https://github.com/Dziubutkus/lottery-smart-contract into Resettable-lottery-smart-contract"
    This reverts commit 69b9aca656c42ef71a7e6621cda9395d8309de13, reversing
    changes made to c0b28b1478ea5d016670a05fb6b1b69897992ca1.

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -99,10 +99,8 @@ contract Lottery is Pausable {
     */
     function cancelLottery() public onlyOwner {
         endingTime = block.timestamp;
-        for(uint i = 0; i < uniqueOnwers; i++) { //  Checks-Effects-Interactions pattern (https://solidity.readthedocs.io/en/develop/security-considerations.html#re-entrancy)
-            uint refundAmount = ownerTicketCount[uniqueTicketOwners[i]] * ticketPrice;
-            ownerTicketCount[uniqueTicketOwners[i]] = 0;
-            uniqueTicketOwners[i].transfer(refundAmount);
+        for(uint i = 0; i < uniqueOnwers; i++) {
+            uniqueTicketOwners[i].transfer(ownerTicketCount[uniqueTicketOwners[i]] * ticketPrice);
         }
         emit LotteryCanceled();
         _cleanLottery();
@@ -139,9 +137,9 @@ contract Lottery is Pausable {
         return uint(keccak256(abi.encodePacked(endingTime, block.timestamp, block.number))) % ticketsSold;
     }
 
-    // function withdrawBalance() public onlyOwner {
-    //     msg.sender.transfer(address(this).balance);
-    // }
+    function withdrawBalance() public onlyOwner {
+        msg.sender.transfer(address(this).balance);
+    }
 
     function lotteryEnded() public view returns(bool){
         return _lotteryEnded();
