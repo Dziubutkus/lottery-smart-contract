fd84f9d24f8f2c5ef1ba66d5903e8cb95d68dcf3
dziubutkus <dziubutkus@gmail.com>
2018-12-21 23:12:22 -0700

    remove ethereum bridge variable

--- truffle-config.js
+++ truffle-config.js
@@ -1,22 +1,15 @@
-//const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 
 module.exports = {
   networks: {
     development: {
       host: 'localhost',
-      port: 7545,
-      network_id: '*', // eslint-disable-line camelcase
-    },
-    /*
-    ganache: {
-      host: 'localhost',
-      port: 7545,
+      port: 8545,
       network_id: '*', // eslint-disable-line camelcase
     },
     rinkeby: {
       provider: () => new HDWalletProvider('dziugo walletas', "https://rinkeby.infura.io/507b091b9fc14b08ae4bdc6d05b89ebc"),
       network_id: "4"
     },
-    */
   },
 };
